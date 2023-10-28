4b27924679cc9a9383999b1228f94e9434f0d625
Omeir Mohamed Omar Haroon <oharoon@c1r5s2.42lisboa.com>
2023-10-28 21:48:24 +0100

    prototype for missing function in exec.c

--- src/builtins/exec.c
+++ src/builtins/exec.c
@@ -6,16 +6,22 @@
 /*   By: oharoon <oharoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 21:14:28 by pedromota         #+#    #+#             */
-/*   Updated: 2023/10/28 16:18:41 by oharoon          ###   ########.fr       */
+/*   Updated: 2023/10/28 21:47:40 by oharoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	**list_2_matrix(t_env *env)
+{
+	void(*env);
+	return(NULL);
+}
+
 int	ft_exec(t_cmds *node)
 {
 	standard_choiser(node);
-	if (execve(node->args[0], node->args, node->sh->env->vars) == -1)
+	if (execve(node->args[0], node->args, list_2_matrix(node->sh->env->vars)) == -1)
 	{
 		perror(node->args[0]);
         clean_commands(&node->sh->cmds);
