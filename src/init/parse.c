64d90fceedf6187f2262f6d5bf1fdbe45f9f4cb7
PedroVMota <pedromvmota@outlook.pt>
2023-12-02 18:23:53 +0000

    removing empty files

--- src/init/parse.c
+++ src/init/parse.c
@@ -6,7 +6,7 @@
 /*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 08:52:30 by pedro             #+#    #+#             */
-/*   Updated: 2023/12/02 17:46:41 by pedro            ###   ########.fr       */
+/*   Updated: 2023/12/02 18:22:57 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,21 @@ void	parse(t_cmds *node, t_shell *sh)
 		redirection(command, sh);
 		command = command->next;
 	}
+}
+
+void	remove_quotes(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == '\1' || str[i] == '\2')
+			i++;
+		else
+			str[j++] = str[i++];
+	}
+	str[j] = '\0';
 }
